1160a35c1c6f4ba944b7f3ad02b07614859741d2
kur0tenh <angel.sink@hotmail.com>
2015-09-22 12:35:03 -0500

    Control
    Booleano del control

--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Rotation : MonoBehaviour {
-
+	public bool Control= false;
 	// Use this for initialization
 	void Start () {
 
@@ -10,6 +10,16 @@ public class Rotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.rotation = Quaternion.Euler(0, 0, ((Mathf.Atan2(Input.GetAxis("RotationX"),Input.GetAxis("RotationY")))* Mathf.Rad2Deg));
+
+		if (Control) {
+			this.transform.rotation = Quaternion.Euler (0, 0, ((Mathf.Atan2 (Input.GetAxis ("RotationX"), Input.GetAxis ("RotationY"))) * Mathf.Rad2Deg));
+		} else {
+			if (Input.GetKey ("left")) {
+				transform.Rotate(0, 0,2);
+			}
+			if (Input.GetKey ("right")) {
+				transform.Rotate(0, 0, -2);
+			}
+		}
 	}
 }
