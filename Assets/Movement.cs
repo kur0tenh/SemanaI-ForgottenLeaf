dd3144c4ed633892b2e519b970c92816697eb1b3
kur0tenh <angel.sink@hotmail.com>
2015-09-22 11:35:09 -0500

    Inicio del proyecto
    :dancer:

---
+++ Assets/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float MovementSensitivity = 2;
+	Vector2 Move = new Vector2 (0,0);
+	// Use this for initialization
+	void Start () {
+		MovementSensitivity *= 0.1f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0);
+		this.transform.Translate(Move*MovementSensitivity);
+	}
+}
