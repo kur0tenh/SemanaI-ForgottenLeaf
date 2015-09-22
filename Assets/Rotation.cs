8ac78ca1488f066d2e60886cc131c3cb9a6b7689
kur0tenh <angel.sink@hotmail.com>
2015-09-22 12:48:11 -0500

    GitWorking

--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Rotation : MonoBehaviour {
 	public bool Control= false;
+	public float rotationVelocity = 5f;
 	// Use this for initialization
 	void Start () {
 
@@ -15,10 +16,10 @@ public class Rotation : MonoBehaviour {
 			this.transform.rotation = Quaternion.Euler (0, 0, ((Mathf.Atan2 (Input.GetAxis ("RotationX"), Input.GetAxis ("RotationY"))) * Mathf.Rad2Deg));
 		} else {
 			if (Input.GetKey ("left")) {
-				transform.Rotate(0, 0,2);
+				transform.Rotate(0, 0,rotationVelocity);
 			}
 			if (Input.GetKey ("right")) {
-				transform.Rotate(0, 0, -2);
+				transform.Rotate(0, 0,-rotationVelocity);
 			}
 		}
 	}
