88d9b01615301b507c63ba44b117ed37c88965f0
kur0tenh <angel.sink@hotmail.com>
2015-09-23 23:51:52 -0500

    2

--- Assets/creatEnemy.cs
+++ Assets/creatEnemy.cs
@@ -6,6 +6,8 @@ public class creatEnemy : MonoBehaviour {
 	float width = 0;
 
 	private int randy;
+	private int mrandy;
+
 	public GameObject enemy;
 	public GameObject enemy1;
 	public GameObject enemy2;
@@ -30,7 +32,13 @@ public class creatEnemy : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-
+		mrandy =Random.Range (0, 100);
+		if ((mrandy % 2) == 0) {
+			this.transform.localPosition = new Vector3 (height+2,Random.Range(-width,width));
+		}
+		if ((mrandy % 3) == 0) {
+			this.transform.localPosition = new Vector3 (-height-2,Random.Range(-width,width));
+		}
 		randy = Random.Range (0, 400);
 		
 		if (randy == 1) {
